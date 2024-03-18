c2a3175fcce5cfccd8f60cd53f4579565698d978
NagKothapalli <nag022@gmail.com>
2024-03-18 21:53:12 +0530

    March 18th code pushed

--- src/gmailAutomation/GoogleMail.java
+++ src/gmailAutomation/GoogleMail.java
@@ -1,5 +1,6 @@
 package gmailAutomation;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,12 +11,17 @@ public class GoogleMail
 {
 	
 	String expectedTitle = "Gmail";
+	ChromeDriver driver  = new ChromeDriver(); //opens an empty google chrome browser
 	
-	@Test
+	public GoogleMail()
+	{		
+		System.out.println("Constructor of the GoogleMail class Executed");
+	}
+	
+	@Before           //@Test
 	public void launchGmailApplication()
 	{
-		System.out.println("Test : Launch Gmail Application");
-		ChromeDriver driver  = new ChromeDriver();
+		System.out.println("Test : Launch Gmail Application");		
 		driver.get("https://gmail.com");
 		String sessionID = driver.getWindowHandle();
 		System.out.println("Session ID of the window :" + sessionID);
@@ -31,21 +37,47 @@ public class GoogleMail
 		else
 		{
 			System.out.println("Launch Application Failed");
-		}
-		
-		driver.findElement(By.name("identifier1")).sendKeys("nag022@gmail.com , password");
-		
+		}		
 	}
+	
+	//Execution Flow : Class variables -> Constructor  -> Before method -> Test Method
+	//org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element
+	
+	//By class Methods : ID , Name , Class Name , Tag Name , LinkText , Partial LinkText , Xpath
 	
 	@Test
 	public void loginToGmailApplication()
 	{
 		System.out.println("Test Case : Login to Gmail Application");
+		//driver.findElement(By.name("identifier")).sendKeys("nag022@gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("nag022@gmail.com");
+		driver.findElements(By.className("VfPpkd-vQzf8d")).get(1).click();
 	}
 	
+	@Test
+	public void forgotEmail()
+	{
+		System.out.println("Test Case : Forgot Email");
+		//driver.findElement(By.tagName("button")).click();
+		driver.findElements(By.tagName("button")).get(0).click();
+	}
 	
+	@Test
+	public void learnMore()
+	{
+		System.out.println("Test Case : Learn more about using Guest mode");
+		//driver.findElement(By.linkText("Learn more about using Guest mode")).click();
+		//driver.findElement(By.tagName("a")).click();
+		driver.findElements(By.tagName("a")).get(0).click();
+	}
 	
-	
+	@Test
+	public void createAccount() throws InterruptedException
+	{
+		System.out.println("Test Case : Create Account");
+		Thread.sleep(2000);
+		driver.findElements(By.className("VfPpkd-vQzf8d")).get(2).click();
+	}
 	
 	
 	
