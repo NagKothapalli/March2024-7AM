aefa32e2975464fce1a812daa936bf1f439321b5
NagKothapalli <nag022@gmail.com>
2024-03-22 21:54:30 +0530

    March22nd code pushed

--- src/seleniumAutomation/GoToMeeting.java
+++ src/seleniumAutomation/GoToMeeting.java
@@ -1,23 +1,64 @@
 package seleniumAutomation;
 
+import java.time.Duration;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 public class GoToMeeting
 {
 	
 	ChromeDriver driver = new ChromeDriver();
 	@Test
-	public void registerWithGoToMeeting()
+	public void registerWithGoToMeeting() throws InterruptedException
 	{
 		driver.get("https://www.goto.com/meeting");
+		driver.manage().window().maximize();
 		driver.findElement(By.xpath("//img[@alt='close icon']")).click();
-		driver.findElement(By.xpath("//a[@data-cta-name='Try Free']")).click();
+		Thread.sleep(5000);
+		Actions actions = new Actions(driver);
+		//Every function in actions class will return the object of actions class again
+		WebElement tryfree = driver.findElement(By.xpath("//a[@data-cta-name='Try Free']"));
+		actions.moveToElement(tryfree).click().build().perform();
+		driver.findElement(By.xpath("//img[@alt='close icon']")).click();
+		//driver.findElement(By.xpath("//a[@data-cta-name='Try Free']")).click();
 		driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("Nag");
-		driver.findElement(By.xpath("//input[@name='lastName']")).sendKeys("K");
+		driver.findElement(By.xpath("//input[@name='lastName']")).sendKeys("Kothapalli");
 		driver.findElement(By.xpath("//input[@name='email']")).sendKeys("nag022@gmail.com");
-		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Abcd@123");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Abcd@123");		
+		WebElement companysize = driver.findElement(By.xpath("//select[@id='CompanySize']"));
+		actions.moveToElement(companysize).build().perform();
+		Thread.sleep(2000);
+		Select myselect = new Select(companysize);
+		myselect.selectByIndex(2);
+		Thread.sleep(2000);
+		myselect.selectByVisibleText("250 - 499");		
+		WebElement startBtn = driver.findElement(By.xpath("//button[text()='Start for Free']"));
+		actions.moveToElement(startBtn).pause(Duration.ofSeconds(2)).click().build().perform();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//input[@placeholder='Enter Activation Code']")).sendKeys("123456");
+		driver.findElement(By.xpath("//button[text()='Verify']")).click();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
